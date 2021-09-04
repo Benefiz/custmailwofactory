1fd390bd1fab9be57b1af2f9e8a2f66e9eaa4470
Ekkasit <boom300742@hotmail.com>
2021-09-04 10:54:39 +0700

    Add factory

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -17,20 +17,8 @@ public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
-    }
+    private static Customer customer;
+
     public String generateMail() {
         return customer.createMail();
     }
@@ -38,20 +26,21 @@ public class CustomerMailApplication {
     public static void main(String[] args) {
         CustomerMailApplication app = new CustomerMailApplication();
         Scanner inp = new Scanner(System.in);
-        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
-        int type = inp.nextInt();
-        switch(type) {
-            case 1:
-                app.getCustomerTypeFromUser("Regular");
-                break;
-            case 2:
-                app.getCustomerTypeFromUser("Mountain");
-                break;
-            case 3:
-                app.getCustomerTypeFromUser("Delinquent");
-                break;
-            
+        System.out.println("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
+        int typeNumber = inp.nextInt();
+        String type = "";
+        switch(typeNumber){
+                case 1:
+                    type = "Regular";
+                    break;
+                case 2:
+                    type = "Mountain";
+                    break;
+                case 3:
+                    type = "Delinquent";
+                    break;
         }
+        customer = CustomerFactory.createCustomer(type);
         System.out.println(app.generateMail());        
     }
 }
